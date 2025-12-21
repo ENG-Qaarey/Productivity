3b64dd6b99256910b1234bdce3595906ba20bab6
burakorkmez <burakorkmezz@gmail.com>
2025-12-21 16:01:00 +0800

    feat: enhance user authentication and synchronization with Clerk; add axios and react-query for API handling

---
+++ frontend/src/hooks/useUserSync.js
@@ -0,0 +1,26 @@
+import { useAuth, useUser } from "@clerk/clerk-react";
+import { useMutation } from "@tanstack/react-query";
+import { useEffect } from "react";
+import { syncUser } from "../lib/api";
+
+// the best way to imptlement this is by using webhooks
+function useUserSync() {
+  const { isSignedIn } = useAuth();
+  const { user } = useUser();
+
+  const { mutate: synUserMutation, isPending, isSuccess } = useMutation({ mutationFn: syncUser });
+
+  useEffect(() => {
+    if (isSignedIn && user && !isPending && !isSuccess) {
+      synUserMutation({
+        email: user.primaryEmailAddress.emailAddress,
+        name: user.fullName || user.firstName,
+        imageUrl: user.imageUrl,
+      });
+    }
+  }, [isSignedIn, user, synUserMutation, isPending, isSuccess]);
+
+  return { isSynced: isSuccess };
+}
+
+export default useUserSync;
